218f7c58cc76670056ff368c7e8f3e859847f40a
Tomer Faran <tomer.faran@gmail.com>
2024-12-27 18:20:26 +0200

    MCTS
    Co-authored-by: koralshilon <koralshilon@users.noreply.github.com>

--- src/MSTC.py
+++ src/MSTC.py
@@ -1,3 +1,5 @@
+from itertools import permutations
+from random import randint, shuffle
 from src.strategies import Strategy
 from src.piece import Piece
 from src.colour import Colour
@@ -11,17 +13,16 @@ global_time_limit = None
 
 
 class MonteCarloTreeSearchNode(Strategy):
-    def __init__(self, state, parent=None, parent_action=None):
+
+    def __init__(self, state, parent=None, parent_action=None, child_boards=None):
         self.state = state # Represent the current Board
         self.parent = parent # None for the root
         self.parent_action = parent_action
         self.children = [] # Data structure might need to change, hold all possible moves 
         self._number_of_visits = 0 # Number of times we visited a node
-        self._results = defaultdict(int) # Holds the result from each traverse 
-        self._results[1] = 0 # Number of wins
-        self._results[-1] = 0 # Number of losses
-        self._untried_actions = None #
-        self._untried_actions = self.untried_actions() # list of moves we haven't explored yet
+        self.wins_losses = 0
+        self._untried_actions = child_boards # list of moves we haven't explored yet
+        self.colour = None
 
 
     @staticmethod
@@ -36,9 +37,15 @@ class MonteCarloTreeSearchNode(Strategy):
         start_time = time.time()
 
         root = MonteCarloTreeSearchNode(state=board)
+        self._untried_actions = self.generate_boards(board, colour, dice_rolls) # List of all possible boards for the player with the current dice rolls
+        if len(self._untried_actions) == 0:
+            print("Didn't generate any boards.\n")
+        self.colour = colour
+
         selected_node = root.best_action()
+
         
-        for move in optimal_move: # Callback for handle_move
+        for move in selected_node.parent_action: # Callback for handle_move
             make_move(move['piece_at'], move['die_roll'])
 
     
@@ -95,21 +102,12 @@ class MonteCarloTreeSearchNode(Strategy):
         return resulting_boards
             
 
-    def untried_actions(self):
-        self._untried_actions = self.state.get_legal_actions()
+    def untried_actions(self,):
         return self._untried_actions
     
 
     def n(self):
         return self._number_of_visits
-    
-
-    def expand(self):
-        action = self._untried_actions.pop()
-        next_state = self.state.move(action)
-        child_node = MonteCarloTreeSearchNode(next_state, parent=self, parent_action=action)
-        self.children.append(child_node)
-        return child_node 
         
         
     def is_terminal_node(self):
@@ -117,27 +115,47 @@ class MonteCarloTreeSearchNode(Strategy):
 
 
     def rollout(self):
-        current_rollout_state = self.state
-        while not current_rollout_state.has_game_ended():
-            possible_moves = current_rollout_state.get_legal_actions()
-            action = self.rollout_policy(possible_moves)
-            current_rollout_state = current_rollout_state.move(action)
-        return current_rollout_state.game_result()
+        current_rollout_state = self.state.create_copy() # Board according to the move we decided to do, might need to copy it.
+        colour = self.colour
+        while not current_rollout_state.has_game_ended(): # or time ended
+            dice_roll = [randint(1, 6), randint(1, 6)]
+            if dice_roll[0] == dice_roll[1]:
+                dice_roll = [dice_roll[0]] * 4
+
+            for die_roll in dice_roll:
+                valid_pieces = current_rollout_state.get_pieces(colour)
+                shuffle(valid_pieces) # makes the random action
+                for piece in valid_pieces:
+                    if current_rollout_state.is_move_possible(piece, die_roll):
+                        current_rollout_state.move_piece(piece.location, die_roll)
+                        break
+            colour = Colour.other()
+
+        return current_rollout_state.game_result(current_rollout_state)
 
 
     def backpropagate(self, result):
         self._number_of_visits += 1.
-        self._results[result] += 1.
+        self.wins_losses += result
         if self.parent:
             self.parent.backpropagate(result)
 
 
     def is_fully_expanded(self):
         return len(self._untried_actions) == 0
+    
 
+    def expand(self):
+        next_state, action = next(iter(self._untried_actions.items()))
+        print(f"move that led to next state is {action}") 
+        del(self._untried_actions[next_state]) # Delete the key-value pair of the untried_actions field
+        child_node = MonteCarloTreeSearchNode(next_state, parent=self, parent_action=action,)
+        self.children.append(child_node)
+        return child_node 
 
-    def best_child(self, c_param=0.1):
-        choices_weights = [(c.q() / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
+        
+    def best_child(self, c_param=0.1): #UCB, need to multiply -1 if its the opponent's turn
+        choices_weights = [(c.wins_losses / c.n()) + c_param * np.sqrt((2 * np.log(self.n()) / c.n())) for c in self.children]
         return self.children[np.argmax(choices_weights)]
 
 
@@ -149,6 +167,7 @@ class MonteCarloTreeSearchNode(Strategy):
         current_node = self
         while not current_node.is_terminal_node():
             if not current_node.is_fully_expanded():
+                
                 return current_node.expand()
             else:
                 current_node = current_node.best_child()
@@ -157,6 +176,7 @@ class MonteCarloTreeSearchNode(Strategy):
 
     def best_action(self):
         simulation_no = 100
+        # Might need to insert here time limit check
         for i in range(simulation_no):
             v = self._tree_policy()
             reward = v.rollout()
@@ -164,10 +184,15 @@ class MonteCarloTreeSearchNode(Strategy):
         return self.best_child(c_param=0.)
     
 
-    def game_result(self):
+    def game_result(self, current_rollout_state):
         '''
-        Modify according to your game or 
-        needs. Returns 1 or 0 or -1 depending
-        on your state corresponding to win,
-        tie or a loss.
+        Returns score depending
+        on your state corresponding to win, a loss or mars.
         '''
+        # Check for mars
+        if len(current_rollout_state.get_pieces(self.colour) == 0 and current_rollout_state.get_pieces(self.colour.other) == 15):
+            return 2
+        elif len(current_rollout_state.get_pieces(self.colour) == 15 and current_rollout_state.get_pieces(self.colour.other) == 0):
+            return -2
+        # Regular win
+        return 1 if len(current_rollout_state.get_pieces(self.colour)) == 0 else -1
